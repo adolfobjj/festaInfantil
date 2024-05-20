65b3bcf9900de7d15085e97f7eb9c5139b269b5a
adolfobjj <Luiz@2532>
2024-05-20 09:25:53 -0300

    Criação do projeto

---
+++ src/main/java/br/com/festaInfantil/demo/AppFestaInfantilApplication.java
@@ -0,0 +1,13 @@
+package br.com.festaInfantil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFestaInfantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFestaInfantilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/festaInfantil/demo/AppFestaInfantilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.festaInfantil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFestaInfantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
